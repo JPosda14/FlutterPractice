24d2a97a1b921dcdf7ae17bd4fa02d6922f6a2ee
Juan Andres Posada <jposada1026@cue.edu.co>
2025-01-31 22:04:37 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main(){
   
-  runApp (MyApp());
+  runApp (const MyApp());
 }
 
 class MyApp extends StatelessWidget{
+
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(child: Text('Hola mundo')),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen()
     );
   }
 
